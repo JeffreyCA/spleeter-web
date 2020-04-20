dc1fd10311b756ac818c59a005c21e8f2ac04026
JeffreyCA <jeffreyca16@gmail.com>
2020-04-20 15:04:27 -0400

    Update key

--- django_react/settings.py
+++ django_react/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = os.getenv('SECRET_KEY', 'default')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
