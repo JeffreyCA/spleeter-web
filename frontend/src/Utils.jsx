edbf05df8a77a126a2e6a757ee5ef3d80c11ab41
JeffreyCA <jeffreyca16@gmail.com>
2020-05-04 14:40:43 -0400

    Revert Utils change

--- frontend/src/Utils.jsx
+++ frontend/src/Utils.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { DateTime } from 'luxon'
 
+const SEC_PER_MIN = 60
 const DAYS_PER_WEEK = 7
 
 /**
@@ -9,10 +10,13 @@ const DAYS_PER_WEEK = 7
 export const toRelativeDateSpan = isoDateString => {
   const dateTime = DateTime.fromISO(isoDateString)
   const longDateTimeStr = dateTime.toLocaleString(DateTime.DATETIME_MED)
+  const diffSeconds = -dateTime.diffNow().as('seconds')
   const diffDays = -dateTime.diffNow().as('days')
   var label
 
-  if (diffDays < DAYS_PER_WEEK) {
+  if (diffSeconds < SEC_PER_MIN) {
+    label = 'moments ago'
+  } else if (diffDays < DAYS_PER_WEEK) {
     label = dateTime.toRelative()
   } else {
     label = dateTime.toLocaleString(DateTime.DATE_MED)
