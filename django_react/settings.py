f4530027483a06bcdaa1e02c33468ef239300e57
JeffreyCA <jeffreyca16@gmail.com>
2020-05-09 12:29:27 -0400

    Enable DEBUG flag for now

--- django_react/settings.py
+++ django_react/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', 'default')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['spleeter-web.herokuapp.com']
 
