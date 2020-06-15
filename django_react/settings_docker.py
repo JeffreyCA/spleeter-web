184ce530e6f197fc88ff0aace10e357d5d50a43e
JeffreyCA <jeffreyca16@gmail.com>
2020-06-15 09:20:29 -0400

    Fix Docker settings

--- django_react/settings_docker.py
+++ django_react/settings_docker.py
@@ -103,10 +103,6 @@ WSGI_APPLICATION = 'django_react.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600)
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
 }
 
 # Internationalization
@@ -142,4 +138,4 @@ STATICFILES_DIRS = (
 
 # Override production variables if DJANGO_DEVELOPMENT env variable is set
 if os.getenv('DJANGO_DEVELOPMENT'):
-    from .settings_dev import *
+    from .settings_docker_dev import *
