f2e26748e49f7bf644bb55664608c968858862c3
JeffreyCA <jeffreyca16@gmail.com>
2020-06-15 09:20:29 -0400

    Fix Docker settings

---
+++ django_react/settings_docker_dev.py
@@ -0,0 +1,27 @@
+import dj_database_url
+
+SECRET_KEY = 'default'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost']
+
+# DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
+# OR
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://spleeter-web@127.0.0.1:5432/spleeter-web', conn_max_age=600)
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
