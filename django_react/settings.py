be08ee0060327c7a706b91270e543e8b465ebb9a
JeffreyCA <jeffreyca16@gmail.com>
2020-08-10 22:29:05 -0400

    Add endpoint for YouTube search

--- django_react/settings.py
+++ django_react/settings.py
@@ -1,13 +1,9 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'default')
+YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
