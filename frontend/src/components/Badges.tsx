d9cab207a70d246ab7b9d97eb64c3199972fabe2
JeffreyCA <jeffreyca16@gmail.com>
2021-01-13 10:53:54 -0500

    Update react-music-player

--- frontend/src/components/Badges.tsx
+++ frontend/src/components/Badges.tsx
@@ -4,11 +4,13 @@ import { Badge } from 'react-bootstrap';
 interface BadgeProps {
   className?: string;
   faded?: boolean;
+  title?: string;
 }
 
-export const OriginalBadge = (): JSX.Element => {
+export const OriginalBadge = (props: BadgeProps): JSX.Element => {
+  const { title } = props;
   return (
-    <Badge className="ml-2 mr-2" pill variant="primary">
+    <Badge className="ml-2 mr-2" pill variant="primary" title={title}>
       Original
     </Badge>
   );
@@ -23,45 +25,45 @@ export const AllBadge = (): JSX.Element => {
 };
 
 export const VocalsBadge = (props: BadgeProps): JSX.Element => {
-  const { faded } = props;
+  const { faded, title } = props;
   return (
-    <Badge pill className={props.className} variant={faded ? 'vocals-faded' : 'vocals'}>
+    <Badge pill className={props.className} variant={faded ? 'vocals-faded' : 'vocals'} title={title}>
       Vocals
     </Badge>
   );
 };
 
 export const AccompBadge = (props: BadgeProps): JSX.Element => {
-  const { faded } = props;
+  const { faded, title } = props;
   return (
-    <Badge pill className={props.className} variant={faded ? 'accomp-faded' : 'accomp'}>
+    <Badge pill className={props.className} variant={faded ? 'accomp-faded' : 'accomp'} title={title}>
       Accompaniment
     </Badge>
   );
 };
 
 export const AccompShortBadge = (props: BadgeProps): JSX.Element => {
-  const { faded } = props;
+  const { faded, title } = props;
   return (
-    <Badge pill className={props.className} variant={faded ? 'accomp-faded' : 'accomp'}>
+    <Badge pill className={props.className} variant={faded ? 'accomp-faded' : 'accomp'} title={title}>
       Accomp.
     </Badge>
   );
 };
 
 export const DrumsBadge = (props: BadgeProps): JSX.Element => {
-  const { faded } = props;
+  const { faded, title } = props;
   return (
-    <Badge pill className={props.className} variant={faded ? 'drums-faded' : 'drums'}>
+    <Badge pill className={props.className} variant={faded ? 'drums-faded' : 'drums'} title={title}>
       Drums
     </Badge>
   );
 };
 
 export const BassBadge = (props: BadgeProps): JSX.Element => {
-  const { faded } = props;
+  const { faded, title } = props;
   return (
-    <Badge pill className={props.className} variant={faded ? 'bass-faded' : 'bass'}>
+    <Badge pill className={props.className} variant={faded ? 'bass-faded' : 'bass'} title={title}>
       Bass
     </Badge>
   );
