8e6b0018f373537ce3d0c2a5baba2d9d105ada0a
JeffreyCA <jeffreyca16@gmail.com>
2021-05-25 19:33:47 -0700

    Update Demucs separator to use audio adapter

--- api/separators/demucs_separator.py
+++ api/separators/demucs_separator.py
@@ -4,8 +4,10 @@ from pathlib import Path
 import torch
 from billiard.exceptions import SoftTimeLimitExceeded
 from billiard.pool import Pool
+from demucs.pretrained import load_pretrained
 from demucs.separate import *
 from django.conf import settings
+from spleeter.audio.adapter import AudioAdapter
 
 """
 This module defines a wrapper interface over the Demucs API.
@@ -25,39 +27,35 @@ class DemucsSeparator:
         shifts=5
     ):
         assert (model_name in VALID_MODELS)
+        model_name = 'demucs48_hq'
         self.device = 'cpu' if cpu_separation else 'cuda'
+        self.sample_rate = 44100
         self.model_name = model_name
         self.model_file = f'{model_name}.th'
         self.model_dir = Path('pretrained_models')
         self.model_file_path = self.model_dir / self.model_file
         self.shifts = shifts
         self.split = True
+        self.overlap = 0.25
         self.verbose = True
         self.bitrate = bitrate
+        self.audio_adapter = AudioAdapter.default()
 
-    def download_and_verify(self):
-        sha256 = PRETRAINED_MODELS.get(self.model_file)
+    def get_model(self):
+        torch.hub.set_dir(str(self.model_dir))
+        if self.model_file_path.is_file():
+            model = load_model(self.model_file_path)
+        else:
+            model = load_pretrained(self.model_name)
+        model.to(self.device)
 
-        if not self.model_file_path.is_file():
-            if sha256 is None:
-                raise Exception(f"No pretrained model {self.model_file}")
+        return model
 
-            self.model_dir.mkdir(exist_ok=True, parents=True)
-            url = BASE_URL + self.model_file
-            print(
-                "Downloading pre-trained model weights, this could take a while..."
-            )
-            download_file(url, self.model_file_path)
-
-        if sha256 is not None:
-            verify_file(self.model_file_path, sha256)
-
-    def apply_model(self, input_path: Path):
+    def apply_model(self, model, input_path: Path):
         """Applies model to waveform file"""
-        model = load_model(self.model_file_path).to(self.device)
         print(f"Separating track {input_path}")
         wav = AudioFile(input_path).read(streams=0,
-                                         samplerate=44100,
+                                         samplerate=self.sample_rate,
                                          channels=2).to(self.device)
         # Round to nearest short integer for compatibility with how MusDB load audio with stempeg.
         wav = (wav * 2**15).round() / 2**15
@@ -67,6 +65,7 @@ class DemucsSeparator:
                                   wav,
                                   shifts=self.shifts,
                                   split=self.split,
+                                  overlap=self.overlap,
                                   progress=True)
         raw_sources = raw_sources * ref.std() + ref.mean()
         if not settings.CPU_SEPARATION:
@@ -84,8 +83,8 @@ class DemucsSeparator:
         :param output_path: Path to output file
         """
         input_path = Path(input_path)
-        self.download_and_verify()
-        raw_sources = self.apply_model(input_path)
+        model = self.get_model()
+        raw_sources = self.apply_model(model, input_path)
 
         final_source = None
 
@@ -96,13 +95,12 @@ class DemucsSeparator:
 
             final_source = source if final_source is None else final_source + source
 
-        final_source = (final_source * 2**15).clamp_(-2**15, 2**15 - 1).short()
         final_source = final_source.cpu().transpose(0, 1).numpy()
 
-        encode_mp3(final_source,
-                   str(output_path),
-                   self.bitrate,
-                   verbose=self.verbose)
+        print('Exporting MP3...')
+        self.audio_adapter.save(output_path, final_source, self.sample_rate,
+                                'mp3', self.bitrate)
+
 
     def separate_into_parts(self, input_path: str, output_path: str):
         """Creates a dynamic mix
@@ -112,8 +110,8 @@ class DemucsSeparator:
         """
         input_path = Path(input_path)
         output_path = Path(output_path)
-        self.download_and_verify()
-        raw_sources = self.apply_model(input_path)
+        model = self.get_model()
+        raw_sources = self.apply_model(model, input_path)
 
         # Export all source MP3s in parallel
         pool = Pool()
@@ -121,14 +119,14 @@ class DemucsSeparator:
 
         for source, name in zip(raw_sources,
                                 ['drums', 'bass', 'other', 'vocals']):
-            source = (source * 2**15).clamp_(-2**15, 2**15 - 1).short()
+
             source = source.cpu().transpose(0, 1).numpy()
             filename = f'{name}.mp3'
 
             print(f'Exporting {name} MP3...')
-            task = pool.apply_async(encode_mp3,
-                                    (source, str(output_path / filename),
-                                     self.bitrate, self.verbose))
+            task = pool.apply_async(self.audio_adapter.save,
+                                    (output_path / filename, source,
+                                     self.sample_rate, 'mp3', self.bitrate))
             tasks.append(task)
 
         try:
