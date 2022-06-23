7b902623f78bc2bb98894f085e9a6d468b3dd44a
JeffreyCA <jeffreyca16@gmail.com>
2022-06-23 09:16:02 -0700

    Show loading spinner on Export button

--- frontend/src/components/Mixer/ExportButton.tsx
+++ frontend/src/components/Mixer/ExportButton.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, OverlayTrigger, Spinner, Tooltip } from 'react-bootstrap';
 import { BoxArrowUpRight } from 'react-bootstrap-icons';
+import { OverlayInjectedProps } from 'react-bootstrap/esm/Overlay';
 
 interface Props {
   className?: string;
   disabled: boolean;
+  error?: string;
+  loading: boolean;
   onClick: () => void;
 }
 
@@ -13,17 +16,49 @@ interface Props {
  */
 class ExportButton extends React.Component<Props> {
   render(): JSX.Element {
-    return (
+    let content = null;
+    if (this.props.loading) {
+      content = <Spinner animation="border" />;
+    } else {
+      content = (
+        <>
+          <BoxArrowUpRight className="align-middle" />
+          <span className="ml-1 align-middle">Export</span>
+        </>
+      );
+    }
+
+    const renderTooltip = (props: OverlayInjectedProps) => (
+      <Tooltip id="button-tooltip" {...props}>
+        {this.props.error}
+      </Tooltip>
+    );
+
+    const disabledStyle: React.CSSProperties = {
+      cursor: 'not-allowed',
+      pointerEvents: 'none',
+    };
+
+    let button = (
       <Button
         className={`text-btn ${this.props.className}`}
         variant="success"
         size="sm"
-        disabled={this.props.disabled}
-        onClick={this.props.onClick}>
-        <BoxArrowUpRight className="align-middle" />
-        <span className="ml-1 align-middle">Export</span>
+        onClick={this.props.onClick}
+        style={this.props.disabled ? disabledStyle : {}}>
+        {content}
       </Button>
     );
+
+    if (this.props.error) {
+      button = (
+        <OverlayTrigger placement="right" overlay={renderTooltip}>
+          <div style={{ display: 'inline-block', cursor: 'not-allowed' }}>{button}</div>
+        </OverlayTrigger>
+      );
+    }
+
+    return button;
   }
 }
 
